b1ee70d0ee7391f048f1381d07fce9a6c38a9bc8
Yokke <mdembelepro@gmail.com>
2024-09-07 19:39:45 +0200

    feat: users entity and tests are done #4 #6

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,4 +15,53 @@ namespace Entity;
  */
 class User
 {
+    public int $id;
+    
+    /**
+     * Summary of __construct
+     * @param string $name
+     * @param string $password
+     */
+    public function __construct(public string $name,public string $password)
+    {
+
+    }
+
+    /**
+     * Summary of setId
+     * @param int $id
+     * @return void
+     */
+    public function setId(int $id):void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Summary of getId
+     * @return int
+     */
+
+    public function getId():int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Summary of getName
+     * @return string
+     */
+    public function getName():string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Summary of getPassword
+     * @return string
+     */
+    public function getPassword():string
+    {
+        return $this->password;
+    }
 }
